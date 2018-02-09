7271ef3979ca5c4b1d6a9cdf071e17a6fee7ec15
Tiago Martinho <martinho@thoughtworks.com>
2018-02-09 18:20:04 +0100

    add input output mapping

--- UnitTestBundle/TestExample.swift
+++ UnitTestBundle/TestExample.swift
@@ -10,18 +10,34 @@ class TestExample: XCTestCase {
     }
     
     func testExample() {
-        sut.methodToTest()
-        XCTAssert(true)
-    }
-    
-    func testPerformanceExample() {
-        self.measure {
-            sut.methodToTest()
-        }
+        let input = """
+                    3
+                    ---+-++- 3
+                    +++++ 4
+                    -+-+- 4
+                    """
+
+        let output = sut.methodToTest(input)
+
+        let expected = """
+                        Case #1: 3
+                        Case #2: 0
+                        Case #3: IMPOSSIBLE
+                        """
+        XCTAssertEqual(expected, output)
     }
 }
 
 class SystemUnderTest {
-    func methodToTest() {
+    func methodToTest(_ input: String) -> String {
+        let inputs = input.split(separator: "\n").dropFirst()
+        var result = ""
+        for (index, _) in inputs.enumerated() {
+            result.append("Case #\(index + 1): ")
+            if index < inputs.count - 1 {
+                result.append("\n")
+            }
+        }
+        return result
     }
 }
